2393a6e1e94fbea7c8848d227c5fe6919620b028
Juan Salgado <juansutt@hotmail.com>
2015-06-09 08:50:22 -0700

    Create getdailyreport.php

---
+++ servicios/getdailyreport.php
@@ -0,0 +1,33 @@
+<?php 
+require_once('../accesodatos/catalogos.php');
+	require_once('/../clases/produced.php');
+	require_once('/../clases/delivery.php');
+    header('Access-Control-Allow-Origin: *');
+    
+    if (isset($_GET['date']) & isset($_GET['subcontractor']))
+        {
+            $subassy = Catalogo :: DailyReport($_GET['date'],$_GET['subcontractor']);
+            $json = '{"materials" : [';
+		    $primero =  true;
+            foreach($subassy as $sub)
+            {
+                $delivery = new Delivery($sub->getModelId()->getId(), $_GET['date']);
+                $produced = new Produced($sub->getModelId()->getId(), $_GET['date']);
+                if (!$primero) $json .= ','; else $primero = false;
+                $json .= '	{ 
+                                    "subcontractor": "'.$sub->getPoId()->getSubcontractorId()->getName().'",
+                                    "model" : '.$sub->getModelId()->getNumber().',
+                                    "line" : "'.$sub->getModelId()->getIdLine()->getDescription().'",
+                                    "sapNo" : "'.$sub->getNumber().'",
+                                    "description" : "'.$sub->getDescription().'",
+                                    "lot" : '.$sub->getModelId()->getLot().',
+                                    "produced" : '.$produced->getDaily().',
+                                    "delivered" : '.$delivery->getDaily().',
+                                    "totalprod" : '.$produced->getSum().',
+                                    "totaldeliv" : '.$delivery->getSum().'
+                            }';
+            }
+            $json .= ' ] }';
+    }
+    echo $json;
+ ?>
