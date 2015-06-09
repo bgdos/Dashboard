d206527b3c1432b114abe43eaab8ef80f83b4548
Juan Salgado <juansutt@hotmail.com>
2015-06-09 08:42:00 -0700

    Update servicios.php

--- js/servicios.php
+++ js/servicios.php
@@ -14,10 +14,19 @@ var urlUpdateProfile = 'servicios/updateprofile.php';
 var urlDeleteSchedule = 'servicios/deleteschedule.php';
 var urlEditSchedule = 'servicios/updateschedule.php';
 var urlSavePo = 'servicios/savepo.php';
+var urlSaveUser = 'servicios/saveuser.php';
 var urlUpdatePo = 'servicios/updatepo.php';
+var urlSaveProduction = 'servicios/saveproduction.php';
+var urlSaveDelivery = 'servicios/savedelivery.php';
+var urlUpdateUser = 'servicios/updateuser.php';
 var urlDeletePo = 'servicios/deletepo.php';
 var urlMaterialProduction = 'servicios/getmaterialproduction.php';
 var urlDelivery = 'servicios/getdelivery.php';
+var urlDeliveryUpdate = 'servicios/updatedelivery.php';
+var urlConfirmDelivery = 'servicios/confirmdelivery.php';
+var urlDeliveryConfirm = 'servicios/getdeliveryconfirm.php';
+var urlUser = 'servicios/getuser.php';
+var urlDeleteUser = 'servicios/deleteuser.php';
 var prueba = 'prueba.php';
 var token ='';
 var x = new XMLHttpRequest();
@@ -41,7 +50,7 @@ function loadSchedules(line, date)
 					//parsear a JSON
 					var respuestaJSON = JSON.parse(respuesta);
 					var models = respuestaJSON.models;
-					if (window.location.href == 'http://localhost:8080/production_dashboard/main.html')
+					if (window.location.hash == '')
 						loadScheduleInfo(models);
 					else if (window.location.hash == "#se")
 						ScheduleEditInfo(models);
@@ -126,26 +135,8 @@ function ScheduleAdd(data)
 				 }
 				});
 }
-function ScheduleEdit()
+function ScheduleEdit(data)
 {
-	openprocessing();
-	var position = saveIdSchedule();
-	var data = [];
-	var tb = document.getElementById('schedule-edit');
-	var filas = tb.getElementsByTagName("tr");
-	var index = JSON.parse(sessionStorage.index)
-	for (var i = 0; i < position.length; i++)
-		{
-			id= position[i];
-			model= filas[index[i]].children[2].children[0].value;
-			owner = filas[index[i]].children[3].children[0].value.toUpperCase();
-			lot = filas[index[i]].children[4].children[0].value;
-			line = filas[index[i]].children[5].children[0].value;
-			sDate = filas[index[i]].children[6].children[0].value;
-			data.push( {id: id, line: line, model : model, owner:owner, lot: lot, date: sDate})
-			
-		}
-	
 	var url = urlServer + urlEditSchedule;
 		 $.ajax({ type: "POST",
 		 url: url,
@@ -167,7 +158,7 @@ function ScheduleEdit()
 function ScheduleDelete()
 {
 	openprocessing();
-	var deleteschedule = saveIdSchedule();
+	var deleteschedule = saveIds();
 	var url = urlServer + urlDeleteSchedule;
 	 $.ajax({ type: "POST",
 		 url: url,
@@ -281,36 +272,43 @@ function poAdd(data)
 function editPo()
 {
 	openprocessing();
-	
 	var data = [];
 	var tb = document.getElementById('purchase-edit');
 	var filas = tb.getElementsByTagName("tr");
-	id= po[0].id;
-	for (var i = 0; i < filas.length; i++)
-		{
-			numberId = po[i].materialId;
-			number = filas[i].children[2].children[0].value;
-			qty = filas[i].children[4].children[0].value;
-			model = filas[i].children[5].children[0].value;
-			data.push( {id: numberId, number: number, qty : qty, model:model})
-			
-		}
-	
-	var url = urlServer + urlUpdatePo;
-		 $.ajax({ type: "POST",
-		 url: url,
-		 data: {pos : data},//no need to call JSON.stringify etc... jQ does this for you
-		 cache: false,
-		 success: function(response){//check response: it's always good to check server output when developing...
-			 		 response = JSON.parse(response);
-					 console.log(response.message);
-			 		if (response.status ==0)
-					 	{$( ".ajax" ).load( "po-edit.html");MaterialDescription();}
-			 		 closeprocessing();
-					 popInfo('Information', response.message);
-			 		
-				 }
-				});
+	if (filas.length > 1)
+	{
+		id= po[0].id;
+		for (var i = 0; i < filas.length; i++)
+			{
+				numberId = po[i].materialId;
+				number = filas[i].children[2].children[0].value;
+				qty = filas[i].children[4].children[0].value;
+				model = filas[i].children[5].children[0].value;
+				data.push( {id: numberId, number: number, qty : qty, model:model})
+
+			}
+
+		var url = urlServer + urlUpdatePo;
+			 $.ajax({ type: "POST",
+			 url: url,
+			 data: {pos : data},//no need to call JSON.stringify etc... jQ does this for you
+			 cache: false,
+			 success: function(response){//check response: it's always good to check server output when developing...
+						 response = JSON.parse(response);
+						 console.log(response.message);
+						if (response.status ==0)
+							{$( ".ajax" ).load( "po-edit.html");MaterialDescription();}
+						 closeprocessing();
+						 popInfo('Information', response.message);
+
+					 }
+					});
+	}
+	else
+	{
+		closeprocessing();
+		popInfo('Error', 'PO information not found.');
+	}
 }
 function poDelete(id)
 {
@@ -332,22 +330,6 @@ function poDelete(id)
 			}
 				x.send();
 }
-function updateProfilePassword(password, email)
-{
-        var url = urlServer + urlUpdateProfile + "?email=" + email + "&password=" + password;
-			x.open('GET', url, true);
-			x.onreadystatechange = function()
-			{
-				if (x.status == 200 & x.readyState == 4)
-				{
-					var respuesta = x.responseText;
-					//parsear a JSON
-					var respuestaJSON = JSON.parse(respuesta);
-					popInfo('My Profile', respuestaJSON.message);
-				}
-			}
-				x.send();
-}
 function materialProduction(model)
 {
         var url = urlServer + urlMaterialProduction + "?model=" + model;
@@ -359,7 +341,7 @@ function materialProduction(model)
 					var respuesta = x.responseText;
 					//parsear a JSON
 					var respuestaJSON = JSON.parse(respuesta);
-					if (respuestaJSON.length > 0)
+					if (respuestaJSON.materials.length > 0)
 					{
 						var materials = respuestaJSON.materials;
                     	productionConfirm(materials);
@@ -372,6 +354,61 @@ function materialProduction(model)
 				}
 			}
 				x.send();
+}
+/* save production */
+function productionSave (data) {
+	var url = urlServer + urlSaveProduction;
+	$.ajax({ type: "POST",
+	url: url,
+	data: {materials : data},//no need to call JSON.stringify etc... jQ does this for you
+	cache: false,
+	success: function(response){//check response: it's always good to check server output when developing...
+			 response = JSON.parse(response);
+			 console.log(response.message);
+			 closeprocessing();
+			 if (response.status == 0)
+				{
+					if (sessionStorage.option == 1)
+						$( ".ajax" ).load( "production-confirm.html");
+					if (sessionStorage.option == 2)
+						$( ".ajax" ).load( "production-adjust.html");
+				}
+			 popInfo('Information', response.message, 3000);
+			 }
+		});
+}
+function DeliverySave (data) {
+	var url;
+		if (sessionStorage.option == 3)
+			url = urlServer + urlSaveDelivery;
+		if (sessionStorage.option == 5)
+			url = urlServer + urlDeliveryUpdate;
+		if (sessionStorage.option == 4)
+			url = urlServer + urlConfirmDelivery;//cambiar
+	
+	$.ajax({ type: "POST",
+	url: url,
+	data: {materials : data},//no need to call JSON.stringify etc... jQ does this for you
+	cache: false,
+	success: function(response){//check response: it's always good to check server output when developing...
+			 response = JSON.parse(response);
+			 console.log(response.message);
+			 closeprocessing();
+			 if (response.status == 0)
+				{
+					if (sessionStorage.option == 3)
+						$( ".ajax" ).load( "delivery-new.html");
+					if (sessionStorage.option == 5)
+						$( ".ajax" ).load( "delivery-edit.html");
+					if (sessionStorage.option == 4)
+					{
+						var packing = data[0]["packing"];
+						$( ".ajax" ).load( "receiving-confirm.html");
+					}
+				}
+			 popInfo('Information', response.message, 3000);
+			 }
+		});
 }
 function delivery(po)
 {
@@ -386,6 +423,136 @@ function delivery(po)
 					var respuestaJSON = JSON.parse(respuesta);
 					var materials = respuestaJSON.materials;
                     productionConfirm(materials);
+				}
+			}
+				x.send();
+}
+function deliveryConfirm(packing)
+{
+    var url = urlServer + urlDeliveryConfirm + "?packing="+packing;
+			x.open('GET', url, true);
+			x.onreadystatechange = function()
+			{
+				if (x.status == 200 & x.readyState == 4)
+				{
+					var respuesta = x.responseText;
+					//parsear a JSON
+					var respuestaJSON = JSON.parse(respuesta);
+					var materials = respuestaJSON.materials;
+                    productionConfirm(materials);
+				}
+			}
+				x.send();
+}
+
+function userService(email)
+{
+    var url = urlServer + urlUser + "?user="+email;
+			x.open('GET', url, true);
+			x.onreadystatechange = function()
+			{
+				if (x.status == 200 & x.readyState == 4)
+				{
+					var respuesta = x.responseText;
+					//parsear a JSON
+					var respuestaJSON = JSON.parse(respuesta);
+					var users = respuestaJSON.user;
+                    userEdit(users);
+				}
+			}
+				x.send();
+}
+function userAdd(data)
+{
+	var url = urlServer + urlSaveUser;
+	$.ajax({ type: "POST",
+	url: url,
+	data: {users : data},//no need to call JSON.stringify etc... jQ does this for you
+	cache: false,
+	success: function(response){//check response: it's always good to check server output when developing...
+			 response = JSON.parse(response);
+			 console.log(response.message);
+			 closeprocessing();
+			 if (response.status ==0)
+				{$( ".ajax" ).load( "user-new.html");}
+			 popInfo('Information', response.message, 3000);
+			 }
+		});
+}
+function userDelete(id)
+{
+	openprocessing();
+	var url = urlServer + urlDeleteUser + '?id='+id;
+	 x.open('GET', url, true);
+			x.onreadystatechange = function()
+			{
+				if (x.status == 200 & x.readyState == 4)
+				{
+					var respuesta = x.responseText;
+					//parsear a JSON
+					var response = JSON.parse(respuesta);
+					if (response.status == 0)
+						{$( ".ajax" ).load( "user-delete.html");}
+					closeprocessing();
+					popInfo('Information', response.message);
+				}
+			}
+				x.send();
+}
+
+function editUser()
+{
+	 var email = document.getElementById('f-email').value;
+	
+	var email2,  password, name, lastname, typeId, subcontractor;
+        var userArray = [];
+
+        if (document.getElementById('passwordEdit').value != null)
+            password= document.getElementById('passwordEdit').value;
+        else
+            password = '';
+		email2 = document.getElementById('email').value;
+        name = document.getElementById('name').value;
+        lastname = document.getElementById('lastname').value;
+        typeId = document.getElementById('selectType').value;
+        subcontractor = document.getElementById('selectSubcontractor').value;
+    
+        if (email != '' & email2 != ''  & name != '' & lastname != '' & typeId > 0 & subcontractor > 0 )
+            {	
+                userArray.push({email: email, password : password, name: name, lastname: lastname, typeId: typeId, subcontractor: subcontractor, email2 : email2});
+            }
+        else
+            {
+                closeprocessing();
+            }
+	var url = urlServer + urlUpdateUser;
+		 $.ajax({ type: "POST",
+		 url: url,
+		 data: {user : userArray},//no need to call JSON.stringify etc... jQ does this for you
+		 cache: false,
+		 success: function(response){//check response: it's always good to check server output when developing...
+			 		 response = JSON.parse(response);
+					 console.log(response.message);
+			 		if (response.status ==0)
+					 	{$( ".ajax" ).load( "user-edit.html")}
+			 		 closeprocessing();
+					 popInfo('Information', response.message);
+			 		
+				 }
+				});
+}
+function updateProfilePassword(password, email)
+{
+        var url = urlServer + urlUpdateProfile + "?email=" + email + "&password=" + password;
+			x.open('GET', url, true);
+			x.onreadystatechange = function()
+			{
+				if (x.status == 200 & x.readyState == 4)
+				{
+					var respuesta = x.responseText;
+					//parsear a JSON
+					var respuestaJSON = JSON.parse(respuesta);
+					popInfo('My Profile', respuestaJSON.message);
 				}
 			}
 				x.send();
