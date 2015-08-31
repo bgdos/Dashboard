a774c3d22c5d8799064cce1972426bf64194ae18
Juan Salgado <juansutt@hotmail.com>
2015-08-31 10:26:56 -0700

    Update updateschedule.php
    When update the start date to a future date, the status of the model change to open.

--- servicios/updateschedule.php
+++ servicios/updateschedule.php
@@ -1,7 +1,8 @@
 <?php 
 	require_once('../clases/model.php');
 	header('Access-Control-Allow-Origin: *');
-
+    date_default_timezone_set('America/Tijuana');
+    $date = date('Y-m-d');
     if (isset($_POST['models']))
 	{
 		 $models = $_POST['models'];
@@ -11,7 +12,11 @@
 			$m = new Model();
 			for ($i = 0; $i < count($models); $i++ )
 			{
-				$instruccion .= "update model set number = ".$models[$i]['model'] .",  owner = '".$models[$i]['owner']."',  lot = ".$models[$i]['lot'].",  sdate = '".$models[$i]['date']."', line_id =".$models[$i]['line']." where id = ".$models[$i]['id'].";";
+				
+                if ($date >= $models[$i]['date'])
+                    $instruccion .= "update model set number = ".$models[$i]['model'] .",  owner = '".$models[$i]['owner']."',  lot = ".$models[$i]['lot'].",  sdate = '".$models[$i]['date']."', line_id =".$models[$i]['line'].", status_id = 2 where id = ".$models[$i]['id'].";";
+                else
+                    $instruccion .= "update model set number = ".$models[$i]['model'] .",  owner = '".$models[$i]['owner']."',  lot = ".$models[$i]['lot'].",  sdate = '".$models[$i]['date']."', line_id =".$models[$i]['line'].", status_id = 1 where id = ".$models[$i]['id'].";";
 			
 			}
 			if ($m->editSchedule($instruccion) === true)
