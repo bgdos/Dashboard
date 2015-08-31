55f17c3f25f31fd17a9c2a548903a03a6d31cf69
Juan Salgado <juansutt@hotmail.com>
2015-08-31 10:43:07 -0700

    Update savepo.php
    Error: Al capturar varias ordenes de compra solo se grababa la primer orden y en ella se incluian todos los materiales.
    Solucion: Modificar las condiciones para que se grabe correctamente.

--- servicios/savepo.php
+++ servicios/savepo.php
@@ -3,25 +3,55 @@
 	header('Access-Control-Allow-Origin: *');
     if (isset($_POST['pos']))
 	{
-		 $pos = $_POST['pos'];
+		$pos = $_POST['pos'];
+        $error = false;
 		if ($pos != null)
 		{
-			$instruccion ="";
-			$m = new Material();
-			for ($i = 0; $i < count($pos); $i++ )
-			{
-				$instruccion .= "INSERT INTO po (id, date, subcontractor_Id, status_Id)";
-				$instruccion .= "VALUES(". $pos[$i]['po'] .", '". $pos[$i]['date'] ."', ". $pos[$i]['subcontractor'] .", 1);";
-				$instruccion .= "INSERT INTO material (po_Id, number,description, qty, model_Id)";
-				$instruccion .= "VALUES(". $pos[$i]['po'] .", ". $pos[$i]['sapnumber'] .", '". $pos[$i]['description'] ."', ". $pos[$i]['qty']. ", ". $pos[$i]['model'].");";
-			
-			}
-			if ($m->savePo($instruccion) === true)
-				echo '{ "status" : 0, "message" : "Data saved successfully." }';
+            for ($i = 0; $i < count($pos); $i++)
+            {
+                $po1 = new Po($pos[$i]['po']);
+                if ($po1->getSubcontractorId()->getId() > 0)
+                {
+                    if ($po1->getSubcontractorId()->getId() != $pos[$i]['subcontractor'])
+                    {
+                        echo '{ "status" : 3, "message" : "PO"'. $po1->getSubcontractorId()->getId() .'correspond for other vendor." }';
+                        break;
+                    }
+                }
+            }
+            savepo($pos);
 		}
 		else
 			echo '{ "status" : 1, "message" : "Error on data saving." }';
     }
 	else
 		echo '{ "status" : 2, "message" : "Error, no data received." }';
+
+function savepo($pos)
+{
+	$instruccion ="";
+	$m = new Material();
+    $p1 = 0;
+	for ($i = 0; $i < count($pos); $i++ )
+	{
+		$po1 = new Po($pos[$i]['po']);
+		if ($po1->getId() != $pos[$i]['po'])
+		{
+			if  ($p1 != $pos[$i]['po'])
+            {
+                $p1 = $pos[$i]['po'];
+                $instruccion .= "INSERT INTO po (id, date, subcontractor_Id, status_Id)";
+                $instruccion .= "VALUES(". $pos[$i]['po'] .", '". $pos[$i]['date'] ."', ". $pos[$i]['subcontractor'] .", 1);";
+            }
+		}
+		else
+			$instruccion .= "UPDATE po SET status_Id = 1 WHERE id = ".$pos[$i]['po'].';';
+		$instruccion .= "INSERT INTO material (po_Id, number,description, qty, model_Id)";
+		$instruccion .= "VALUES(". $pos[$i]['po'] .", '". $pos[$i]['sapnumber'] ."', '". $pos[$i]['description'] ."', ". $pos[$i]['qty']. ", ". $pos[$i]['model'].");";
+
+	}
+	if ($m->savePo($instruccion) === true)
+		echo '{ "status" : 0, "message" : "Data saved successfully." }';
+	
+}
 ?>
