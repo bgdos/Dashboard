67a5a387310bc6aebf95e0d47b775f7f9d4635b1
Juan Salgado <juansutt@hotmail.com>
2015-08-31 13:48:34 -0700

    Update getmodeldescription.php

--- servicios/getmodeldescription.php
+++ servicios/getmodeldescription.php
@@ -1,23 +1,27 @@
 <?php 
 	require_once('../accesodatos/catalogos.php');
-	require_once('/../clases/material.php');
+	require_once('/../clases/model.php');
     header('Access-Control-Allow-Origin: *');
-    
-    
-            $mod = Catalogo :: descriptionModel();
-            $json = '{"models" : [';
-		    $primero =  true;
-            foreach($mod as $m)
-            {
-                if (!$primero) $json .= ','; else $primero = false;
-                $json .= '	{ 
-									"id": '.$m->getId().',
-                                    "number": '.$m->getNumber().',
-									"line" : { "id": '.$m->getIdLine()->getId().',
-                                    		   "description": "'.$m->getIdLine()->getDescription().'"}
-                            }';
-            }
-            $json .= ' ] }';
+
+    date_default_timezone_set('America/Tijuana');
+    $date = date('Y-m-d');
+    $mm = new Model();
+    $instruccion = "UPDATE model SET status_Id = 2 WHERE sdate <= '".$date."';";
+    $mm->updateModelStatus($instruccion);
+    $mod = Catalogo :: descriptionModel();
+    $json = '{"models" : [';
+    $primero =  true;
+    foreach($mod as $m)
+    {
+        if (!$primero) $json .= ','; else $primero = false;
+        $json .= '	{ 
+                            "id": '.$m->getId().',
+                            "number": '.$m->getNumber().',
+                            "line" : { "id": '.$m->getIdLine()->getId().',
+                                       "description": "'.$m->getIdLine()->getDescription().'"}
+                    }';
+    }
+    $json .= ' ] }';
     
     echo $json;
  ?>
